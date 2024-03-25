0504857a985c234fcba02b08204b271f708382ec
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-03-25 13:16:35 -0300

    Create JdbWithoutJniApplication.java

---
+++ JDB_without_JNI/src/main/java/tech/noetzold/JDB_without_JNI/JdbWithoutJniApplication.java
@@ -0,0 +1,13 @@
+package tech.noetzold.JDB_without_JNI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbWithoutJniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbWithoutJniApplication.class, args);
+	}
+
+}
