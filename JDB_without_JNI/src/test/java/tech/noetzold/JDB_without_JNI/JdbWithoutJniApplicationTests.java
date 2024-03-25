8347e8f59057907788f512011e47f05746e928e0
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-03-25 13:16:30 -0300

    Create JdbWithoutJniApplicationTests.java

---
+++ JDB_without_JNI/src/test/java/tech/noetzold/JDB_without_JNI/JdbWithoutJniApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.noetzold.JDB_without_JNI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbWithoutJniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
