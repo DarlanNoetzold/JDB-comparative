b55fff2a44f3a8bfdeffd744c5a2c274b9bedb95
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-03-25 13:16:37 -0300

    Create InMemoryNoSqlDb.java

---
+++ JDB_without_JNI/src/main/java/tech/noetzold/JDB_without_JNI/InMemoryNoSqlDb.java
@@ -0,0 +1,58 @@
+package tech.noetzold.JDB_without_JNI;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+public class InMemoryNoSqlDb implements DatabaseInterface {
+    private final Map<String, Document> store = new HashMap<>();
+    private final String dbDirectory = "dbFiles";
+
+    public InMemoryNoSqlDb() {
+        File directory = new File(dbDirectory);
+        if (!directory.exists()) {
+            directory.mkdir();
+        }
+    }
+
+    @Override
+    public void create(String id, Document document) {
+        store.put(id, document);
+        saveToFile(id, document);
+    }
+
+    @Override
+    public Document read(String id) {
+        return store.getOrDefault(id, readFromFile(id));
+    }
+
+    @Override
+    public void update(String id, Document document) {
+        store.put(id, document);
+        saveToFile(id, document);
+    }
+
+    @Override
+    public void delete(String id) {
+        store.remove(id);
+        new File(dbDirectory + "/" + id + ".txt").delete();
+    }
+
+    private void saveToFile(String id, Document document) {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(dbDirectory + "/" + id + ".txt"))) {
+            out.writeObject(document);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Document readFromFile(String id) {
+        try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(dbDirectory + "/" + id + ".txt"))) {
+            return (Document) in.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            return null;
+        }
+    }
+}
