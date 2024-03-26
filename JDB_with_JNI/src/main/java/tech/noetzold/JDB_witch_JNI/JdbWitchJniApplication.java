2181c508169a58041ce748db57f7e59f2af5724c
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2024-03-26 09:10:25 -0300

    Create JdbWitchJniApplication.java

---
+++ JDB_with_JNI/src/main/java/tech/noetzold/JDB_witch_JNI/JdbWitchJniApplication.java
@@ -0,0 +1,13 @@
+package tech.noetzold.JDB_witch_JNI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbWitchJniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdbWitchJniApplication.class, args);
+	}
+
+}
